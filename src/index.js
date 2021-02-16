38b150c83f0d2ab8d6ebd859bee65f28a690f9ad
estydaven <esetnod1111@gmail.com>
2021-02-16 14:19:14 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let string = str;
+  let count = 0;
+
+  for (var i = 0; i < string.length; i++){
+    if(str[i] == '[' || str[i] == '(' || str[i] == '{' || str[i] == '|') {
+        count++;
+    }
+    if (str[i] == ']' || str[i] == ')' || str[i] == '}' || str[i] == '|'){
+        count -= 1;
+    }
+    if (count < 0){
+        break;
+    }
+  }
+    if (count != 0){
+        return false;
+    } else {
+        return true;
+    }
 }
